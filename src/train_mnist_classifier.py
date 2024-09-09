ef8dac04e83bc0839d4634bc9109b4df15f638bd
Vishnu Asutosh Dasu <vishnu98dasu@gmail.com>
2024-09-08 21:48:39 -0400

    update mnist classifier

--- src/train_mnist_classifier.py
+++ src/train_mnist_classifier.py
@@ -42,47 +42,127 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-
 class CNN(nn.Module):
-    def __init__(self, img_dim=(1,28,28), num_classes=10):
+    def __init__(self, channel=1, num_classes=10, net_width=128, net_depth=3, net_act='relu', net_norm='instancenorm', net_pooling='avgpooling', im_size = (32,32)):
         super(CNN, self).__init__()
-        assert img_dim[1] in [28,32]
-        self.fe = torch.nn.Sequential(
-            nn.Conv2d(img_dim[0], 32,3,1),  #26, 28
-            nn.MaxPool2d(2,2),  # 13, 14
-            nn.ReLU(),
-            nn.Dropout2d(0.5), #0.5
-            nn.Conv2d(32,64,3,1),   # 11, 12
-            nn.MaxPool2d(2, 2), # 5, 6
-            nn.ReLU(),
-            nn.Dropout2d(0.5),
-            nn.Conv2d(64, 128, 3, 1),   #3, 4
-            nn.ReLU(),
-            nn.Flatten()
-        )
-        if img_dim[1] == 28:
-            self.cla = torch.nn.Sequential(
-                nn.Linear(1152, 128),
-                nn.ReLU(),
-                nn.Dropout2d(0.5),
-                nn.Linear(128, num_classes),
-            )
-        elif img_dim[1] == 32:
-            self.cla = torch.nn.Sequential(
-                nn.Linear(2048, 128),
-                nn.ReLU(),
-                nn.Dropout2d(0.5),
-                nn.Linear(128, num_classes),
 
-            )
+        self.features, shape_feat = self._make_layers(channel, net_width, net_depth, net_norm, net_act, net_pooling, im_size)
+        num_feat = shape_feat[0]*shape_feat[1]*shape_feat[2]
+        self.classifier = nn.Linear(num_feat, num_classes)
 
     def forward(self, x):
-        fes = self.fe(x)
-        pred = self.cla(fes)
-        return F.log_softmax(pred, dim=1)
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def embed(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        return out
+
+    def _get_activation(self, net_act):
+        if net_act == 'sigmoid':
+            return nn.Sigmoid()
+        elif net_act == 'relu':
+            return nn.ReLU(inplace=True)
+        elif net_act == 'leakyrelu':
+            return nn.LeakyReLU(negative_slope=0.01)
+        elif net_act == 'swish':
+            return Swish()
+        else:
+            exit('unknown activation function: %s'%net_act)
+
+    def _get_pooling(self, net_pooling):
+        if net_pooling == 'maxpooling':
+            return nn.MaxPool2d(kernel_size=2, stride=2)
+        elif net_pooling == 'avgpooling':
+            return nn.AvgPool2d(kernel_size=2, stride=2)
+        elif net_pooling == 'none':
+            return None
+        else:
+            exit('unknown net_pooling: %s'%net_pooling)
+
+    def _get_normlayer(self, net_norm, shape_feat):
+        # shape_feat = (c*h*w)
+        if net_norm == 'batchnorm':
+            return nn.BatchNorm2d(shape_feat[0], affine=True)
+        elif net_norm == 'layernorm':
+            return nn.LayerNorm(shape_feat, elementwise_affine=True)
+        elif net_norm == 'instancenorm':
+            return nn.GroupNorm(shape_feat[0], shape_feat[0], affine=True)
+        elif net_norm == 'groupnorm':
+            return nn.GroupNorm(4, shape_feat[0], affine=True)
+        elif net_norm == 'none':
+            return None
+        else:
+            exit('unknown net_norm: %s'%net_norm)
+
+    def _make_layers(self, channel, net_width, net_depth, net_norm, net_act, net_pooling, im_size):
+        layers = []
+        in_channels = channel
+        if im_size[0] == 28:
+            im_size = (32, 32)
+        shape_feat = [in_channels, im_size[0], im_size[1]]
+        for d in range(net_depth):
+            layers += [nn.Conv2d(in_channels, net_width, kernel_size=3, padding=3 if channel == 1 and d == 0 else 1)]
+            shape_feat[0] = net_width
+            if net_norm != 'none':
+                layers += [self._get_normlayer(net_norm, shape_feat)]
+            layers += [self._get_activation(net_act)]
+            in_channels = net_width
+            if net_pooling != 'none':
+                layers += [self._get_pooling(net_pooling)]
+                shape_feat[1] //= 2
+                shape_feat[2] //= 2
+
+        return nn.Sequential(*layers), shape_feat
 
     def pred(self, x):
-        return F.softmax(self.cla(self.fe(x)), dim=1)
+        return F.softmax(self.forward(x), dim=1)
+
+
+
+# class CNN(nn.Module):
+#     def __init__(self, img_dim=(1,28,28), num_classes=10):
+#         super(CNN, self).__init__()
+#         assert img_dim[1] in [28,32]
+#         self.fe = torch.nn.Sequential(
+#             nn.Conv2d(img_dim[0], 32,3,1),  #26, 28
+#             nn.MaxPool2d(2,2),  # 13, 14
+#             nn.ReLU(),
+#             nn.Dropout2d(0.5), #0.5
+#             nn.Conv2d(32,64,3,1),   # 11, 12
+#             nn.MaxPool2d(2, 2), # 5, 6
+#             nn.ReLU(),
+#             nn.Dropout2d(0.5),
+#             nn.Conv2d(64, 128, 3, 1),   #3, 4
+#             nn.ReLU(),
+#             nn.Flatten()
+#         )
+#         if img_dim[1] == 28:
+#             self.cla = torch.nn.Sequential(
+#                 nn.Linear(1152, 128),
+#                 nn.ReLU(),
+#                 nn.Dropout2d(0.5),
+#                 nn.Linear(128, num_classes),
+#             )
+#         elif img_dim[1] == 32:
+#             self.cla = torch.nn.Sequential(
+#                 nn.Linear(2048, 128),
+#                 nn.ReLU(),
+#                 nn.Dropout2d(0.5),
+#                 nn.Linear(128, num_classes),
+
+#             )
+
+#     def forward(self, x):
+#         fes = self.fe(x)
+#         pred = self.cla(fes)
+#         return F.log_softmax(pred, dim=1)
+
+#     def pred(self, x):
+#         return F.softmax(self.cla(self.fe(x)), dim=1)
 
 class MLP(nn.Module):
 
@@ -180,7 +260,8 @@ def torch_evaluate(train_img, train_label, val_image, val_label, device, arch='M
         train_img = train_img.reshape([num_sample, -1])
         val_image = val_image.reshape([val_image.shape[0], -1])
     else:
-        net = CNN(img_dim, num_classes=val_label.max().item()+1).cuda()
+        # net = CNN(img_dim, num_classes=val_label.max().item()+1).cuda()
+        net = CNN().cuda()
 
     n_train = int(num_sample*0.9)
     if num_sample < 1000:
